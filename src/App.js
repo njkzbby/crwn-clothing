9cd54e12d86f906beb254ea23dc13692901573cf
njkzbby <njkzbby@yandex.ru>
2021-12-25 20:15:41 +0500

    finish styling menu-item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import HomePage from './homepage-component';
+import HomePage from './pages/homepage/homepage.component';
 function App() {
   return (
     <div >
