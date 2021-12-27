fc3302bdde947bf85276d89ef301521a1c53828a
njkzbby <njkzbby@yandex.ru>
2021-12-27 22:53:44 +0500

    updating a shop page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
+
 function App() {
   return (
-    <div >
-<HomePage />
-    </div>
+    <Router>
+      <Routes >
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
+      </Routes>
+    </Router>
   );
 }
 
