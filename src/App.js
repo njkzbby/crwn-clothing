fb45acb1252a2ae8056a4caf73f1629d0f64c712
njkzbby <njkzbby@yandex.ru>
2021-12-27 23:21:18 +0500

    add header

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <Router>
-      <Routes >
-        <Route path="/" element={<HomePage />} />
-        <Route path="/shop" element={<ShopPage />} />
-      </Routes>
+      <div>
+        <Header />
+        <Routes >
+          <Route path="/" element={<HomePage />} />
+          <Route path="/shop" element={<ShopPage />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
